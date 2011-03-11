53c02bc4ee92a0e1009e03d1edac153300a65d7b
Maciej Szerling <maciej.szerling@polidea.pl>
2011-03-11 14:17:16 +0100

    decreased sdk version in default.properties fixed NPE described on: http://stackoverflow.com/questions/5272706/nullpointerexception-in-drawables-mutate-method-android-1-6/5273281#5273281

---
+++ src/pl/polidea/treeview/demo/TreeViewListDemo.java
@@ -0,0 +1,144 @@
+package pl.polidea.treeview.demo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import pl.polidea.treeview.InMemoryTreeStateManager;
+import pl.polidea.treeview.R;
+import pl.polidea.treeview.TreeBuilder;
+import pl.polidea.treeview.TreeNodeInfo;
+import pl.polidea.treeview.TreeStateManager;
+import pl.polidea.treeview.TreeViewList;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+
+/**
+ * Demo activity showing how the tree view can be used.
+ * 
+ */
+public class TreeViewListDemo extends Activity {
+
+    private final Set<Long> selected = new HashSet<Long>();
+
+    private static final String TAG = TreeViewListDemo.class.getSimpleName();
+    private TreeViewList treeView;
+
+    private static final int[] DEMO_NODES = new int[] { 0, 0, 1, 1, 1, 2, 2, 1,
+            1, 2, 1, 0, 0, 0, 1, 2, 3, 2, 0, 0, 1, 2, 0, 1, 2, 0, 1 };
+    private static final int LEVEL_NUMBER = 4;
+    private final TreeStateManager<Long> manager = new InMemoryTreeStateManager<Long>();
+    private final TreeBuilder<Long> treeBuilder = new TreeBuilder<Long>(manager);
+    private FancyColouredVariousSizesAdapter fancyAdapter;
+    private SimpleStandardAdapter simpleAdapter;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        for (int i = 0; i < DEMO_NODES.length; i++) {
+            treeBuilder.sequentiallyAddNextNode((long) i, DEMO_NODES[i]);
+        }
+        Log.d(TAG, manager.toString());
+        treeView = (TreeViewList) findViewById(R.id.mainTreeView);
+        fancyAdapter = new FancyColouredVariousSizesAdapter(this, selected,
+                manager, LEVEL_NUMBER);
+        simpleAdapter = new SimpleStandardAdapter(this, selected, manager,
+                LEVEL_NUMBER);
+        treeView.setAdapter(simpleAdapter);
+        registerForContextMenu(treeView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+        final MenuItem collapsibleMenu = menu
+                .findItem(R.id.collapsible_menu_item);
+        if (treeView.isCollapsible()) {
+            collapsibleMenu.setTitle(R.string.collapsible_menu_disable);
+            collapsibleMenu.setTitleCondensed(getResources().getString(
+                    R.string.collapsible_condensed_disable));
+        } else {
+            collapsibleMenu.setTitle(R.string.collapsible_menu_enable);
+            collapsibleMenu.setTitleCondensed(getResources().getString(
+                    R.string.collapsible_condensed_enable));
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.simple_menu_item:
+            treeView.setAdapter(simpleAdapter);
+            break;
+        case R.id.fancy_menu_item:
+            treeView.setAdapter(fancyAdapter);
+            break;
+        case R.id.collapsible_menu_item:
+            treeView.setCollapsible(!treeView.isCollapsible());
+            break;
+        case R.id.expand_all_menu_item:
+            manager.expandEverythingBelow(null);
+            break;
+        case R.id.collapse_all_menu_item:
+            manager.collapseChildren(null);
+            break;
+        default:
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(final ContextMenu menu, final View v,
+            final ContextMenuInfo menuInfo) {
+        final AdapterContextMenuInfo adapterInfo = (AdapterContextMenuInfo) menuInfo;
+        final long id = adapterInfo.id;
+        final TreeNodeInfo<Long> info = manager.getNodeInfo(id);
+        if (info.isWithChildren()) {
+            final MenuInflater menuInflater = getMenuInflater();
+            menuInflater.inflate(R.menu.context_menu, menu);
+            if (info.isExpanded()) {
+                menu.findItem(R.id.context_menu_expand_item).setVisible(false);
+                menu.findItem(R.id.context_menu_expand_all).setVisible(false);
+            } else {
+                menu.findItem(R.id.context_menu_collapse).setVisible(false);
+            }
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(final MenuItem item) {
+        final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+                .getMenuInfo();
+        final long id = info.id;
+        switch (item.getItemId()) {
+        case R.id.context_menu_collapse:
+            manager.collapseChildren(id);
+            return true;
+        case R.id.context_menu_expand_all:
+            manager.expandEverythingBelow(id);
+            return true;
+        case R.id.context_menu_expand_item:
+            manager.expandDirectChildren(id);
+            return true;
+        default:
+            return super.onContextItemSelected(item);
+        }
+    }
+}
